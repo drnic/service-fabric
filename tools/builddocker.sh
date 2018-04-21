d61a4e03042c139e0b55f0dc267a2f45c604b4b3
Dr Nic Williams <drnicwilliams@gmail.com>
2018-04-21 18:19:43 -0400

    consistent use of $IMAGE_NAME

--- tools/builddocker.sh
+++ tools/builddocker.sh
@@ -4,17 +4,15 @@ set -ex
 CDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 REPO_ROOT=$CDIR/..
 DOCKER_VERSION=`cat $REPO_ROOT/DOCKER_VERSION`
-REGISTRY=microsoft
-IMAGE_NAME=service-fabric-build-ubuntu
-
+IMAGE_NAME=microsoft/service-fabric-build-ubuntu
 
 BUILD_IMAGE_PATH=$REPO_ROOT/Dockerfile
 
-docker build -t $REGISTRY/$IMAGE_NAME:latest -f $BUILD_IMAGE_PATH $REPO_ROOT 
-docker tag $REGISTRY/$IMAGE_NAME:latest $REGISTRY/$IMAGE_NAME:$DOCKER_VERSION
+docker build -t $IMAGE_NAME:latest -f $BUILD_IMAGE_PATH $REPO_ROOT 
+docker tag $IMAGE_NAME:latest $IMAGE_NAME:$DOCKER_VERSION
 
 # Release the official images to the registry.  Credentials required
 if [ "$1" == "release" ]; then
-    docker push $REGISTRY/$IMAGE_NAME:latest
-    docker push $REGISTRY/$IMAGE_NAME:$DOCKER_VERSION
+    docker push $IMAGE_NAME:latest
+    docker push $IMAGE_NAME:$DOCKER_VERSION
 fi
